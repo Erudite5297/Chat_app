653a700fc7ab90223cfc8487b33323c01d284df5
Moirangthem Erudite <moirangthemsumanta64@gmail.com>
2025-11-06 11:32:06 +0530

    cors config

--- fullstack-chat-app-master/backend/src/index.js
+++ fullstack-chat-app-master/backend/src/index.js
@@ -2,30 +2,27 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
-import cors from "cors";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://erudite-project.netlify.app/", "http://localhost:5173"],
     credentials: true,
   })
 );
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
